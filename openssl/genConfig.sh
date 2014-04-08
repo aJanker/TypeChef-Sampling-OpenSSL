54db6f65d1edf475109a9a99cb56ea981fa1c3a4
Andreas Janker <janker@r2d2.fim.uni-passau.de>
2014-04-08 10:00:51 +0200

    configuration generation script

---
+++ openssl/genConfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Start config generation..."
+timeout 600 ./Configure linux-x86_64 $*
+
+RETVAL=$?
+echo $RETVAL
+[ $RETVAL -eq 0 ] && echo "Success_Build"
+[ $RETVAL -eq 124 ] && echo "Failure_Timeout"
+[ $RETVAL -ne 0 ] && echo "Failure_Build"
+
+exit
+
