cfc17831f60b688e5144ab85ffadb27ec7cd3a07
aJanker <cGH!igxhb9>
2014-04-14 21:36:31 +0200

    made verify script

--- openssl/verify.sh
+++ openssl/verify.sh
@@ -1,6 +1,35 @@
 #!/bin/bash 
 
+filesToProcess() {
+  local listFile=configFlags
+  cat $listFile
+}
+
 cd openssl
-echo $(pwd)
+
+#Ref first
+i = 0
+filesToProcess|while read feature; do
+        echo "Verifiying $1"
+        echo "With features: $feature"
+        ./genConfig.sh $feature | tee $i_ref.config
+        ./build.sh | tee $i_ref.build
+        ./runtest.sh | tee $i_ref.test
+        i=`expr $i + 1`
+        done
+
+#Clean
+git checkout .
+
+#Org Next
+i = 0
+filesToProcess|while read feature; do
+	    echo "Verifiying $1"
+        echo "With features: $feature"
+        ./genConfig.sh $feature | tee $i_org.config
+        ./build.sh | tee $i_org.build
+	 	./runtest.sh | tee $i_org.test
+        i=`expr $i + 1`
+        done
 
 exit
