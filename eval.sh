25166ff95b42b3c8bab8c7941c3372de3d159889
Andreas Janker <janker@kitt.fim.uni-passau.de>
2014-04-15 08:42:07 +0200

    updated eval.sh

--- eval.sh
+++ eval.sh
@@ -8,12 +8,7 @@ filesToProcess() {
 }
 
 flags=" --bdd \
-  	--study busybox --reuseAST --refEval rename --refLink $path/CLinking.interface \
-	-I /usr/local/include \
-	-I /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed \
-	-I /usr/lib/gcc/x86_64-linux-gnu/4.6/include \
-	-I /usr/include/x86_64-linux-gnu \
-	-I /usr/include \
+  	--study openssl --reuseAST --refEval rename --refLink $path/CLinking.interface \
         -I $path/openssl \
 	 -I $path/openssl/include
 	-I $path/openssl/include/openssl
@@ -26,8 +21,8 @@ filesToProcess|while read i; do
          echo "Analysing $path/openssl/$i.c"
          echo "With settings: $flags"
          ../TypeChef/cRefactor.sh $path/openssl/$i.c $flags
-         echo "Verifiying $path/openssl/$i.c"
-	 openssl/verify.sh $path/openssl/$i.c
+         echo "Verifiying $path/result/$i"
+	 openssl/verify.sh $path/result/$i
          done
 
 exit
