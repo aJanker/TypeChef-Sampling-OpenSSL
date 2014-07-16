183e8c0ec86447652f63f7015e31d9fb9d162226
Andreas Janker <janker@ett.fim.uni-passau.de>
2014-07-16 12:36:38 +0200

    update preparation script

---
+++ prep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+path=$(cd "$(dirname "$0")"; pwd)
+echo "$path"
+cd $path
+
+filesToProcess() {
+  local listFile=$path/openssl_files
+  cat $listFile | sed "${1}~8p" 
+}
+
+flags=" --bdd \
+  	--study openssl --reuseAST --refPrep --refLink $path/CLinking.interface \
+        -I $path/openssl \
+	 -I $path/openssl/include
+	-I $path/openssl/include/openssl
+	--platfromHeader $path/openssl/platform.h \
+        --featureModelDimacs $path/openssl/OpenSSL.dimacs \
+	--include $path/openssl/partial_configuration.h \
+	--recordTiming --parserstatistics --lexNoStdout"
+
+filesToProcess $1|while read i; do
+         echo "Analysing $path/openssl/$i.c"
+         echo "With settings: $flags"
+         ../Morpheus/morpheus.sh $path/openssl/$i.c $flags
+         done
+
+exit
