a0a952e9f027f57e431b4c4e80a1b6f6c6426cdf
Andreas Janker <janker@ravel.fim.uni-passau.de>
2014-08-11 15:03:50 +0200

    parallel change to inline

--- eval.sh
+++ eval.sh
@@ -10,7 +10,7 @@ filesToProcess() {
 }
 
 flags=" --bdd \
-  	--study openssl --reuseAST --refEval extract --refLink $path/CLinking.interface \
+  	--study openssl --reuseAST --refEval inline --refLink $path/CLinking.interface \
         -I $path/openssl \
 	 -I $path/openssl/include
 	-I $path/openssl/include/openssl
