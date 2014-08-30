beb3c623031157746308e02a90d8a1fff348f121
aJanker <cGH!igxhb9>
2014-08-30 21:33:53 +0200

    cleanup for release

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash 
 
-./eval.sh 1 2>&1 | tee renaming
+path=$(cd "$(dirname "$0")"; pwd)
+
+
+flags=" --bdd \
+  	--study openssl --reuseAST --showGui \
+        -I $path/openssl \
+	-I $path/openssl/include
+	-I $path/openssl/include/openssl
+	--platfromHeader $path/openssl/platform.h \
+        --featureModelDimacs $path/openssl/OpenSSL.dimacs \
+	--include $path/openssl/partial_configuration.h \
+	--recordTiming --parserstatistics --lexNoStdout"
+
+echo "Analysing $path/openssl/$1"
+echo "With settings: $flags"
+../Morpheus/morpheus.sh $path/openssl/$1 $flags
 
 exit
